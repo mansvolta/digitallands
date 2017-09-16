94af7a4c8fc8e1530118499186024e260c3abbea
mansvolta <purple.ink.corporation@gmail.com>
2017-09-15 20:48:45 -0500

    primera intergacion js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+	console.log('hola');
 
+	// var $startContainer = $('.start');
+
+	// $startContainer.height(window.innerHeight);
+
+	// $(window).resize(function(){
+	// 	$startContainer.height(window.innerHeight);
+	// });
 });
