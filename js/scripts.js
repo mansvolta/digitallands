dde385767a4c9c0bcdfcf9b54ea96e0433c2be6d
mansvolta <purple.ink.corporation@gmail.com>
2017-09-26 00:26:33 -0500

    se agrego la funcionalidad de categorias y otros estilos

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	console.log('hola');
 
-	var $startContainer = $('.start'),
-		$aboutContainer = $('.about');
-	$startContainer.height(window.innerHeight);
-	$aboutContainer.height(window.innerHeight);
-	$(window).resize(function(){
-		$startContainer.height(window.innerHeight);
-		$aboutContainer.height(window.innerHeight);
-	});
+	// var $startContainer = $('.start'),
+	// $aboutContainer = $('.about');
+	// $startContainer.height(window.innerHeight);
+	// $aboutContainer.height(window.innerHeight);
+	// $(window).resize(function(){
+	// 	$startContainer.height(window.innerHeight);
+	// 	$aboutContainer.height(window.innerHeight);
+	// });
 
 	var	$navButton = $('.nav-button');
 
@@ -18,4 +18,21 @@ $(document).ready(function(){
 			console.log('scrollin');
 		}
 	});
+
+	var $FilterLinks = $('.filters li'), 
+		$container = $('.work-stuff');
+
+	$FilterLinks.find('a').click(function(){
+		$FilterLinks.removeClass('active');
+
+		$container.isotope({
+			filter: $(this).attr('data-filter'),
+			itemSelector: '.categoria',
+			animationEngine: 'best-available',
+		});
+		$(this).parent().addClass('active');
+		// $('.work-stuff').addClass('bigger');
+		
+		return false;
+		});
 });
